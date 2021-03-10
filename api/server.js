6682f443e8497c0b6cc9b7580a9f97bd27c27202
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-10 13:31:11 -0500

    logger middleware function works

--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
+const MW = require('./middleware/middleware')
 
 server.use(express.json());
 server.use(morgan('dev'));
-server.use(logger);
+server.use(MW.logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
